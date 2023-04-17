85df68c0a4ca4aaded64c4665d8ad6ef7f7d48b6
MrNewbie3 <atharafiaffandy@gmail.com>
2023-04-17 15:52:06 +0700

    dev: refactoring auth code

--- middleware/roleAuth.js
+++ middleware/roleAuth.js
@@ -1,17 +1,12 @@
-const adminAuth = (req, res, next) => {
+const authMiddleware = (role) => (req, res, next) => {
   const user = req.user;
-  if (user.role.toUpperCase() === "ADMIN") {
+  if (user.role.toUpperCase() === role.toUpperCase()) {
     return next();
   }
-  return res.status(401).json({ success: false, message: "Access denied, only admin can access this routes", data: [] });
+  return res.status(401).json({ success: false, message: `Access denied, only ${role} can access this route`, data: [] });
 };
 
-const receptionistAuth = (req, res, next) => {
-  const user = req.user;
-  if (user.role.toUpperCase() === "RECEPTIONIST") {
-    return next();
-  }
-  return res.status(401).json({ success: false, message: "Access denied, only receptionist can access this routes", data: [] });
+module.exports = {
+  adminAuth: authMiddleware("ADMIN"),
+  receptionistAuth: authMiddleware("RECEPTIONIST"),
 };
-
-module.exports = { adminAuth, receptionistAuth };
