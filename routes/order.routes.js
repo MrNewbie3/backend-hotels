8434969218ed7df9271093c7c91da15d61c61e8d
MrNewbie3 <atharafiaffandy@gmail.com>
2023-04-20 15:33:59 +0700

    feat: search query

--- routes/order.routes.js
+++ routes/order.routes.js
@@ -1,13 +1,15 @@
-const express = require("express");
-const router = express.Router();
-const { getOrder, getOrderById, newOrder, updateOrder, deleteOrder } = require("../controllers/orderController");
-const authorization = require("../middleware/auth");
-const authentication = require("../middleware/roleAuth");
+const express = require('express');
 
-router.get("/", authorization, authentication.receptionistAuth, getOrder);
-router.get("/:id", authorization, authentication.receptionistAuth, getOrderById);
-router.post("/", authorization, authentication.receptionistAuth, newOrder);
-router.put("/:id", authorization, authentication.receptionistAuth, updateOrder);
-router.delete("/:id", authorization, authentication.receptionistAuth, deleteOrder);
+const router = express.Router();
+const apiHandler = require('../controllers/orderController');
+const authorization = require('../middleware/auth');
+const authentication = require('../middleware/roleAuth');
+
+router.get('/find', apiHandler.findOrder);
+router.get('/', authorization, authentication.receptionistAuth, apiHandler.getOrder);
+router.get('/:id', authorization, authentication.receptionistAuth, apiHandler.getOrderById);
+router.post('/', authorization, authentication.receptionistAuth, apiHandler.newOrder);
+router.put('/:id', authorization, authentication.receptionistAuth, apiHandler.updateOrder);
+router.delete('/:id', authorization, authentication.receptionistAuth, apiHandler.deleteOrder);
 
 module.exports = router;
